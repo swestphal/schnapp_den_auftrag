bda051dcd2ddbeab2528409c29e74b7444144f83
Simone Westphal <hello@swestphal.io>
2021-05-29 10:22:14 +0200

    feature: Initial folder setup and basic frontpage settings

--- schnappi/schnappi/urls.py
+++ schnappi/schnappi/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apps.core.views import frontpage
 
 urlpatterns = [
+    path('', frontpage, name='frontpage'),
     path('admin/', admin.site.urls),
+
 ]
