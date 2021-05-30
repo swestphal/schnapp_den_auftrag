e2804f71a9752058c70233d8c55d694046dc0be9
Simone Westphal <hello@swestphal.io>
2021-05-30 09:44:47 +0200

    feature: View jobs and related applications

--- schnappi/apps/userprofile/views.py
+++ schnappi/apps/userprofile/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from apps.job.models import Application
+from apps.job.models import Application, Job
+
 
 # Create your views here.
 
@@ -14,8 +15,20 @@ def dashboard(request):
 
 @login_required
 def view_application(request, application_id):
-    application = get_object_or_404(Application, pk=application_id, created_by=request.user)
+    if request.user.userprofile.is_employer:
+        application = get_object_or_404(Application, pk=application_id, job__created_by=request.user)
+    else:
+        application = get_object_or_404(Application, pk=application_id, created_by=request.user)
     context = {
         'application': application
     }
     return render(request, 'userprofile/view_application.html', context)
+
+
+@login_required
+def view_dashboard_job(request, job_id):
+    job = get_object_or_404(Job, pk=job_id, created_by=request.user)
+    context = {
+        'job': job
+    }
+    return render(request, 'userprofile/view_dashboard_job.html', context)
